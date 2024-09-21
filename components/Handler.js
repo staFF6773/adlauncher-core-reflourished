2e31ceca1eb10958bb4db9927848fcc858b2eacb
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-09-21 15:35:43 -0600

    Create Handler.js

---
+++ components/Handler.js
@@ -0,0 +1,4 @@
+module.exports = {
+  Downloader: require('./downloader'), // Exporting the Downloader class from the downloader module
+  Launcher: require('./launcher') // Exporting the Launcher class from the launcher module
+};
